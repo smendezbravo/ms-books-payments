4a36bd5dce56feaf8a4d72f091dfd07a97e60cf7
saramendezz <saramendez.infor01@gmail.com>
2025-06-06 17:56:31 +0200

    Proyecto base generado con Spring Initializr

---
+++ src/main/java/com/relatosdepapel/payments/ms_books_payments/MsBooksPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.relatosdepapel.payments.ms_books_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBooksPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBooksPaymentsApplication.class, args);
+	}
+
+}
